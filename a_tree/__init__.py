5063ecab43e180ebe55a35c442c3cb5e687a15dc
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2026-04-03 14:13:12 -0400

    Use explicit exports

--- a_tree/__init__.py
+++ a_tree/__init__.py
@@ -1,1 +1,29 @@
-from .a_tree import *
+from .a_tree import (
+    ATree,
+    ATreeException,
+    AttributeDefinition,
+    DuplicateAttributeException,
+    Event,
+    EventBuilder,
+    LockException,
+    MismatchingAttributeTypeException,
+    MissingAttributeException,
+    NonExistingAttribute,
+    ParseException,
+    Report,
+)
+
+__all__ = (
+    "ATree",
+    "ATreeException",
+    "AttributeDefinition",
+    "DuplicateAttributeException",
+    "Event",
+    "EventBuilder",
+    "LockException",
+    "MismatchingAttributeTypeException",
+    "MissingAttributeException",
+    "NonExistingAttribute",
+    "ParseException",
+    "Report",
+)
